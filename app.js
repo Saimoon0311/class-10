9d9541144affe527103865169ff85eff96fa7b3b
unknown <you@example.com>
2021-06-18 18:35:45 +0500

    class 10

---
+++ app.js
@@ -0,0 +1,156 @@
+// var x = "enter value";
+// var y = "your value is"
+// var a = +prompt(x,y);
+// var b = a + 10;
+// // console.log ("the output is", + " " + b);
+// document.write("the output is "+ "" + b)
+
+
+// var age= prompt("enter your age");
+// if(age == 14){
+//    alert("your age is equal ");
+// }
+// else if(age < 14){
+//     alert("your age is less")
+// }
+// else{
+//     alert("sorry by");
+// }
+
+
+// var age = +prompt("enter your age")
+// var gender = prompt ("enter your gender")
+// if (age > 18 && gender == "male"){
+//     alert("you are allowed to ride")
+// }
+
+// else{
+//     alert("sorry you can not ride")
+// }
+
+
+
+// var age = +prompt("enter your age")
+// var gender = prompt ("enter your gender")
+// if (age > 18 || gender == "male"){
+//     alert("you are allowed to ride")
+// }
+
+// else{
+//     alert("sorry you can not ride")
+// }
+
+
+
+// var a = "5"
+// if (a === 5){
+//     alert("true")
+// }
+
+// else{
+//     alert("fale")
+// }
+
+
+
+
+
+// var x = 5
+// var y = 10
+// var a = 30
+// var b = 15
+// if (x+y === a-b){
+//     alert("correct")
+// }
+// else{
+//     alert("incorrect")
+// }
+
+
+
+// var a = 15;
+// if (a !== 15){
+//     alert("correct")
+// }
+// else{
+//     alert("incorrect")
+// }
+
+
+// var percent =+prompt("enter your percntage")
+// if (percent >= 80 && percent  <=100){
+//     alert("A+")
+// }
+
+// if (percent >= 70 && percent  <=80){
+//     alert("A")
+// }
+
+// if (percent >= 60 && percent  <=70){
+//     alert("B")
+// }
+
+// if (percent >= 50 && percent  <=60){
+//     alert("C")
+// }
+
+// if (percent >= 40 && percent  <=50){
+//     alert("D")
+// }
+
+// if (percent >= 33 && percent  <=40){
+//     alert("E")
+// }
+
+// else if (percent >= 0 && percent  <=33){
+//     alert("F")
+// }
+
+// else{
+//     alert("you did not enter correct percentage")
+// }
+
+// var name = "ali"
+// var name1 = "mughal"
+// var name2 = "saimoon"
+
+// var students = ["ali", "mughal", "saimoon"]
+
+// // console.log(students[0])
+
+// alert ("wellcome " + students [1])
+
+
+// var names = []
+
+// names [0] = "nabeel"
+
+// console.log (names)
+
+// var students = ["ali", "mughal", "saimoon"]
+// students.pop () remove and value
+// console.log(students)
+
+// var students = ["ali", "mughal", "saimoon"]
+// students.push ("bilal", "zunab") edit end value
+// console.log(students)
+
+
+// var students = ["ali", "mughal", "saimoon"]
+// students.shift () remove from first
+// console.log(students)
+
+
+// var students = ["ali", "mughal", "saimoon"]
+// students.unshift ("bilal","zunab")  add from first
+// console.log(students)
+
+
+// var students = ["ali", "mughal", "saimoon"]
+// students.splice (0,2,"bilal")
+// console.log(students)
+
+var students = ["ali", "mughal", "saimoon","bial","zunaib"]
+var copystudents= students.slice (1,4)
+console.log(students)
+console.log (copystudents)
